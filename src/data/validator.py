30285a9fdd24ff2e6e61e71a23ce330697a1b6a9
Madhu <maarinayani@gmail.com>
2026-04-25 22:06:30 +0530

    askfhasbg

--- src/data/validator.py
+++ src/data/validator.py
@@ -10,7 +10,7 @@ from typing import Dict, List
 import pandas as pd
 from pandera.errors import SchemaErrors
 
-from src.schema.data_schema import IoTDatasetSchema, SchemaSettings
+from data.schema.data_schema import IoTDatasetSchema, SchemaSettings
 
 LOGGER = logging.getLogger(__name__)
 
